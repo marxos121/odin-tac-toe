8cf7042169faa8348e2ab9620357d3e692031de7
marxos121 <marxos121@gmail.com>
2023-03-03 19:38:37 +0000

    link main gameloop to DOM elements

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
 const gameBoard = (function () {
   const _board = Array(9);
   let _filled = 0;
-  let _acceptInput = true;
-  let _res = "nc";
 
   function check() {
     let resLeftDiag = 0; // \
     let resRightDiag = 0; // /
 
     for (let i = 0; i != 3; ++i) {
-      if (!_acceptInput) {
-        return _res;
-      }
       //Check diagonals
       resLeftDiag +=
         _board[i * 3 + i] === " " ? 0 : _board[i * 3 + i] === "X" ? 1 : -1;
@@ -23,13 +18,9 @@ const gameBoard = (function () {
           : -1;
 
       if (resLeftDiag === 3 || resRightDiag === 3) {
-        _acceptInput = false;
-        _res = "X";
-        return res;
+        return "X";
       } else if (resLeftDiag === -3 || resRightDiag === -3) {
-        _acceptInput = false;
-        _res = "O";
-        return _res;
+        return "O";
       }
 
       //Check rows and columns
@@ -42,31 +33,21 @@ const gameBoard = (function () {
           _board[j * 3 + i] === " " ? 0 : _board[j * 3 + i] === "X" ? 1 : -1;
 
         if (resRow === 3 || resCol === 3) {
-          _acceptInput = false;
-          _res = "X";
-          return _res;
+          return "X";
         } else if (resRow === -3 || resCol === -3) {
-          _acceptInput = false;
-          _res = "O";
-          return _res;
+          return "O";
         }
       }
     }
 
     if (_filled === 9) {
-      _acceptInput = false;
-      _res = "draw";
-      return _res;
+      return "draw";
     }
 
-    return _res;
+    return "nc";
   }
 
   function move(move, symbol) {
-    if (_acceptInput) {
-      return false;
-    }
-
     if (_board[move] === " ") {
       _board[move] = symbol;
       ++_filled;
@@ -79,30 +60,54 @@ const gameBoard = (function () {
   function clear() {
     _board.fill(" ");
     _filled = 0;
-    _res = "nc";
-    _acceptInput = true;
+  }
+
+  function print() {
+    _board.forEach((cell) => console.log(cell));
   }
 
   _board.fill(" ");
-  return { move, check, clear };
+  return { move, check, clear, print };
 })();
 
 const game = (function () {
   let _currentPlayer = "O";
+  let _acceptInput = true;
+  let _res = "nc";
 
-  function play() {
-    gameBoard.move(_currentPlayer);
+  function play(cell) {
+    if (!_acceptInput || !gameBoard.move(cell, _currentPlayer)) {
+      return;
+    }
+
     _res = gameBoard.check();
-    _displayResult();
+    _updateDisplay(cell);
+    if (_res != "nc") {
+      _acceptInput = false;
+    }
     _currentPlayer = _currentPlayer === "O" ? "X" : "O";
   }
 
-  function _displayResult() {}
+  function _updateDisplay(cell) {
+    const button = document.querySelector(`.board :nth-child(${cell + 1}`);
+    button.textContent = _currentPlayer;
+  }
 
   function startNew() {
-    let _currentPlayer = "O";
+    _currentPlayer = "O";
+    _acceptInput = true;
+    _res = "nc";
     gameBoard.clear();
   }
 
   return { play, startNew };
 })();
+
+const cells = document.querySelectorAll(".cell");
+let it = 0;
+cells.forEach((cell) => {
+  const index = it++;
+  cell.addEventListener("click", () => {
+    game.play(index);
+  });
+});
