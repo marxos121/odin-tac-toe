d3e123620767719be816e4dd437a0954a27fbfc2
marxos121 <marxos121@gmail.com>
2023-03-03 16:26:57 +0000

    refactor gameBoard class

--- scripts.js
+++ scripts.js
@@ -1,10 +1,38 @@
 const gameBoard = (function () {
   const _board = Array(9);
-  let filled = 0;
+  let _filled = 0;
+  let _acceptInput = true;
+  let _res = "nc";
 
   function check() {
-    //Check rows and columns
+    let resLeftDiag = 0; // \
+    let resRightDiag = 0; // /
+
     for (let i = 0; i != 3; ++i) {
+      if (!_acceptInput) {
+        return _res;
+      }
+      //Check diagonals
+      resLeftDiag +=
+        _board[i * 3 + i] === " " ? 0 : _board[i * 3 + i] === "X" ? 1 : -1;
+      resRightDiag +=
+        _board[i * 3 + (2 - i)] === " "
+          ? 0
+          : _board[i * 3 + (2 - i)] === "X"
+          ? 1
+          : -1;
+
+      if (resLeftDiag === 3 || resRightDiag === 3) {
+        _acceptInput = false;
+        res = "X";
+        return res;
+      } else if (resLeftDiag === -3 || resRightDiag === -3) {
+        _acceptInput = false;
+        res = "O";
+        return res;
+      }
+
+      //Check rows and columns
       let resRow = 0;
       let resCol = 0;
       for (let j = 0; j != 3; ++j) {
@@ -14,44 +42,34 @@ const gameBoard = (function () {
           _board[j * 3 + i] === " " ? 0 : _board[j * 3 + i] === "X" ? 1 : -1;
 
         if (resRow === 3 || resCol === 3) {
-          return "X";
+          _acceptInput = false;
+          _res = "X";
+          return _res;
         } else if (resRow === -3 || resCol === -3) {
-          return "O";
+          _acceptInput = false;
+          _res = "O";
+          return _res;
         }
       }
     }
 
-    //Check diagonals
-    let resLeftDiag = 0; // \
-    let resRightDiag = 0; // /
-    for (let i = 0; i != 3; ++i) {
-      resLeftDiag +=
-        _board[i * 3 + i] === " " ? 0 : _board[i * 3 + i] === "X" ? 1 : -1;
-      resRightDiag +=
-        _board[i * 3 + (2 - i)] === " "
-          ? 0
-          : _board[i * 3 + (2 - i)] === "X"
-          ? 1
-          : -1;
+    if (_filled === 9) {
+      _acceptInput = false;
+      _res = "draw";
+      return _res;
     }
 
-    if (resLeftDiag === 3 || resRightDiag === 3) {
-      return "X";
-    } else if (resLeftDiag === -3 || resRightDiag === -3) {
-      return "O";
-    }
-
-    if (filled === 9) {
-      return "draw";
-    } else {
-      return "nc";
-    }
+    return _res;
   }
 
   function move(move, symbol) {
+    if (_acceptInput) {
+      return false;
+    }
+
     if (_board[move] === " ") {
       _board[move] = symbol;
-      ++filled;
+      ++_filled;
       return true;
     } else {
       return false;
@@ -60,9 +78,11 @@ const gameBoard = (function () {
 
   function clear() {
     _board.fill(" ");
-    filled = 0;
+    _filled = 0;
+    _res = "nc";
+    _acceptInput = true;
   }
 
-  clear();
+  _board.fille(" ");
   return { move, check, clear };
 })();
